9d109569cac0333271b2296bd60e6bec59356cd1
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-10-06 13:04:18 +0800

    added routing

---
+++ src/app/users/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center item-center bg-black">
+    <h1 className='text-white'>Profile Page</h1>
+   </main>
+  )
+}
+
+export default Profile
