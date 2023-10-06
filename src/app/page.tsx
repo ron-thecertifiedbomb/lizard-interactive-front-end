7d4eeaf216186c6bf91639b441825771e49a440a
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-10-06 16:09:50 +0800

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+
 
 export default function Home() {
-  return (
-    <main className="flex h-screen flex-col items-center justify-center item-center bg-black">
-     <h1 className='text-white'>Lizard Interactive Front End Side</h1>
-    </main>
-  )
+  
+
+  const isServer = typeof window === 'undefined';
+  if(isServer){
+   return 'Message from a Server!!!!!'
+  } else {
+    return 'Message from a Client'
+  }
+
 }
+
+
