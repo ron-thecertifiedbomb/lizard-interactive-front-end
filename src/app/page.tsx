64d1e4aeab66e4c5c25d1f76dfc9f5f5addc5fb8
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-10-06 11:08:13 +0800

    added breakpoints in tailwing config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24 bg-black">
+    <main className="flex h-screen flex-col items-center justify-center item-center bg-black">
      <h1 className='text-white'>Lizard Interactive Front End Side</h1>
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'sm': '640px',        
+        'md': '768px',        
+        'lg': '1024px',        
+        'xl': '1280px',       
+        '2xl': '1536px',      
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
