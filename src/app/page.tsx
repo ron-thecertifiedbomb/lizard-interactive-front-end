b3b82fbc7d50389bb246be8659e65ed02649bcc2
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-10-06 11:00:28 +0800

    change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-     <h1 className='text-white'>Lizard Interactive Front End</h1>
+    <main className="flex h-screen flex-col items-center justify-between p-24 bg-black">
+     <h1 className='text-white'>Lizard Interactive Front End Side</h1>
     </main>
   )
 }
