cd734183dff756e0ded2f66836500303935124cd
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-10-08 12:50:12 +0800

    configure eslint

--- src/app/components/productlistrenderer/productlistrenderer.tsx
+++ src/app/components/productlistrenderer/productlistrenderer.tsx
@@ -1,9 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { Product } from "@/app/interfaces/product";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "@/redux/store";
-import { setProductList } from "@/redux/reducers/productReducer";
+
 
 interface ProductListRendererProps {
   productlist: Product[];
@@ -12,30 +10,9 @@ interface ProductListRendererProps {
 const ProductListRenderer: React.FC<ProductListRendererProps> = ({
   productlist,
 }) => {
-  const dispatch = useDispatch();
+  
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dataToRedux = productlist;
-        dispatch(setProductList(dataToRedux));
-        console.log("data from the server to redux", dataToRedux);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [dispatch, setProductList]);
-
-  console.log("data from the server", productlist);
-
-
- const productState = useSelector(
-    (state: RootState) => state.product
-  );
-
-  console.log("Current Redux Data", productState);
+  console.log("Fetch Data from the Server", productlist);
 
   return <div>Product List Renderer Page</div>;
 };
